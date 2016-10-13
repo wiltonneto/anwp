b1c9cb1c7583744e56304f061c8821faa64837b1
wiltonneto <wilton@MacBook-Pro.local>
2016-10-13 11:45:01 -0300

    corrige search box no topo

--- wp-content/themes/pluginhunt/header.php
+++ wp-content/themes/pluginhunt/header.php
@@ -172,7 +172,7 @@ wp_enqueue_style('custom_css', get_template_directory_uri() . '/css/custom_mobil
         <form class="navbar-form navbar-left header--search-form" role="search" action="<?php echo esc_url( home_url('/') );?>">
           <div class="form-group">
             <label class="header--search-form--label" for="search" data-reactid=".0.0.0.0.0"><span><svg width="15" height="15" viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg"><title>Oval 95</title><path d="M9.383 10.347c-.987.78-2.233 1.244-3.588 1.244C2.595 11.59 0 8.997 0 5.796 0 2.595 2.595 0 5.795 0c3.2 0 5.796 2.595 5.796 5.795 0 1.355-.464 2.6-1.243 3.588L15 14.036l-.964.964-4.653-4.653zm-3.588-.12c2.448 0 4.432-1.984 4.432-4.432 0-2.447-1.984-4.43-4.432-4.43-2.447 0-4.43 1.983-4.43 4.43 0 2.448 1.983 4.432 4.43 4.432z" fill="#BBB" fill-rule="evenodd"></path></svg></span></label>
-            <input type="text" class="form-control header--search-form--field" placeholder = "<?php _e('Search..','pluginhunt'); ?>" value="<?php the_search_query(); ?>" name="s" id="s">
+            <input type="text" class="form-control header--search-form--field" placeholder = "<?php _e('Descubra sua próxima novidade...','pluginhunt'); ?>" value="<?php the_search_query(); ?>" name="s" id="s">
           </div>
           <button type="submit" class="btn btn-default hide"><?php _e('Submit','pluginhunt');?></button>
         </form>
